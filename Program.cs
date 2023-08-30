f1d452a7cf299a777c521f402a9611950b76b565
Deividson Omedio <deividsonomedio@outlook.com>
2023-08-29 21:52:40 -0300

    second commit

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>{
 });
 
 builder.Services.AddDbContext<MyDbContext>(options =>
-    options.UseMySql("server=localhost; port=3306; user id=root;password=curso4311;database=MotoGo_DB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql")));
+    options.UseMySql("server=localhost; port=3306; user id=root;password=Senai2022;database=MotoGo_DB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
